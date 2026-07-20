448e288130a3fe622aefa5f647201e22cc53e387
Ewad245 <89444542+Ewad245@users.noreply.github.com>
2026-07-20 14:14:33 +0700

    Update WebPlatformManager.java

--- app/src/main/java/cse311/gui/platform/WebPlatformManager.java
+++ app/src/main/java/cse311/gui/platform/WebPlatformManager.java
@@ -61,7 +61,12 @@ public class WebPlatformManager implements PlatformManager {
         dialog.setHeaderText("Select file to upload (" + ext + ")");
         
         Button uploadBtn = new Button("Choose File...");
-        VBox vbox = new VBox(10, uploadBtn);
+        javafx.scene.control.Label statusLabel = new javafx.scene.control.Label();
+        javafx.scene.control.ProgressBar progressBar = new javafx.scene.control.ProgressBar(0);
+        progressBar.setVisible(false);
+        progressBar.setPrefWidth(200);
+        
+        VBox vbox = new VBox(10, uploadBtn, statusLabel, progressBar);
         vbox.setStyle("-fx-padding: 20; -fx-alignment: center;");
         dialog.getDialogPane().setContent(vbox);
         dialog.getDialogPane().getButtonTypes().addAll(ButtonType.CANCEL);
@@ -83,19 +88,55 @@ public class WebPlatformManager implements PlatformManager {
                     java.util.logging.Logger.getLogger(WebPlatformManager.class.getName()).log(java.util.logging.Level.FINE, "Method setSelectFileOnClick not found", e);
                 }
                 
-                Consumer<File> onFile = file -> {
-                    dialog.setResult(file);
-                    dialog.close();
+                Consumer<File> onFileSelected = file -> {
+                    javafx.application.Platform.runLater(() -> {
+                        statusLabel.setText("Uploading: " + file.getName());
+                        progressBar.setVisible(true);
+                        uploadBtn.setDisable(true);
+                    });
                 };
+                
+                // Track progress
+                try {
+                    javafx.beans.value.ObservableValue<?> progressProp = 
+                        (javafx.beans.value.ObservableValue<?>) uploaderObj.getClass().getMethod("progressProperty").invoke(uploaderObj);
+                    progressProp.addListener((obs, oldVal, newVal) -> {
+                        if (newVal instanceof Number) {
+                            double progress = ((Number) newVal).doubleValue();
+                            javafx.application.Platform.runLater(() -> {
+                                progressBar.setProgress(progress);
+                            });
+                        }
+                    });
+                } catch (NoSuchMethodException e) {
+                    java.util.logging.Logger.getLogger(WebPlatformManager.class.getName()).log(java.util.logging.Level.FINE, "Method progressProperty not found", e);
+                }
+                
+                // Track uploaded file completion
+                try {
+                    javafx.beans.value.ObservableValue<?> uploadedFileProp = 
+                        (javafx.beans.value.ObservableValue<?>) uploaderObj.getClass().getMethod("uploadedFileProperty").invoke(uploaderObj);
+                    uploadedFileProp.addListener((obs, oldVal, newVal) -> {
+                        if (newVal instanceof File) {
+                            File uploadedFile = (File) newVal;
+                            javafx.application.Platform.runLater(() -> {
+                                statusLabel.setText("Upload complete!");
+                                dialog.setResult(uploadedFile);
+                                dialog.close();
+                            });
+                        }
+                    });
+                } catch (NoSuchMethodException e) {
+                    java.util.logging.Logger.getLogger(WebPlatformManager.class.getName()).log(java.util.logging.Level.FINE, "Method uploadedFileProperty not found", e);
+                }
                 
                 boolean methodFound = false;
                 for (java.lang.reflect.Method m : uploaderObj.getClass().getMethods()) {
                     if ("setOnFileSelected".equals(m.getName()) && m.getParameterCount() == 1) {
                         Class<?> paramType = m.getParameterTypes()[0];
                         if (paramType == Consumer.class) {
-                            m.invoke(uploaderObj, onFile);
+                            m.invoke(uploaderObj, onFileSelected);
                             methodFound = true;
-                            // don't break yet, we want to print all methods
                         } else if (paramType.isInterface()) {
                             Object proxy = java.lang.reflect.Proxy.newProxyInstance(
                                 Thread.currentThread().getContextClassLoader(),
@@ -103,9 +144,9 @@ public class WebPlatformManager implements PlatformManager {
                                 (p, method, args) -> {
                                     if (args != null && args.length > 0) {
                                         if (args[0] instanceof File) {
-                                            onFile.accept((File) args[0]);
+                                            onFileSelected.accept((File) args[0]);
                                         } else if (args[0] instanceof String) {
-                                            onFile.accept(new File((String) args[0]));
+                                            onFileSelected.accept(new File((String) args[0]));
                                         }
                                     }
                                     return null;
@@ -113,7 +154,6 @@ public class WebPlatformManager implements PlatformManager {
                             );
                             m.invoke(uploaderObj, proxy);
                             methodFound = true;
-                            // don't break yet, print all methods
                         }
                     }
                 }
